f96c850444367ca04810c22cbffc4e13029b25a8
Thomas Petchel <tpetchel@gmail.com>
2025-11-04 19:24:38 -0800

    Refactor values map for Roman numeral conversion to a single line

--- roman-to-integer/scala/Solution.scala
+++ roman-to-integer/scala/Solution.scala
@@ -1,20 +1,8 @@
 //> using scala 3.5.1
 
 object Solution {
-    private val values = Map(
-        "I" -> 1,
-        "IV" -> 4,
-        "V" -> 5,
-        "IX" -> 9,
-        "X" -> 10,
-        "XL" -> 40,
-        "L" -> 50,
-        "XC" -> 90,
-        "C" -> 100,
-        "CD" -> 400,
-        "D" -> 500,
-        "CM" -> 900,
-        "M" -> 1000)
+    private val values = Map("I" -> 1, "IV" -> 4, "V" -> 5, "IX" -> 9, "X" -> 10, "XL" -> 40, "L" -> 50, "XC" -> 90, 
+        "C" -> 100, "CD" -> 400, "D" -> 500, "CM" -> 900, "M" -> 1000)
 
     def romanToInt(s: String): Int = romanToIntAux(s, 0)
 
